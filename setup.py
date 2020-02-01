bf47e76c6218c0df54505519b3136d25ff952f6c
probinso <pmoss.robinson@gmail.com>
2020-01-31 21:03:55 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='ACOio',
+    version='0.1.0',
+    description='IO tools for the Aloha Cabled Observaroty',
+    py_modules=['ACOio'],
+    package_dir={'': 'src'}
+)
